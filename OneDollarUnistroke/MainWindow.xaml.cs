423c88c38249b1388205a302520a7383a5c30265
Carlson Sharpless <cwsharpless@gmail.com>
2020-03-03 19:28:36 -0500

    Added basic algorithm skeleton.

--- OneDollarUnistroke/MainWindow.xaml.cs
+++ OneDollarUnistroke/MainWindow.xaml.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Windows.Ink;
 
 namespace OneDollarUnistroke
 {
-    /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -26,12 +12,28 @@ namespace OneDollarUnistroke
         }
 
         /// LeftMouseUpHandler - Controls what happens when the left mouse is released.
-        /// The current code slightly zooms in the window, but this is just to test that the handler works.
+        /// This means that the algorithm is run.
         private void LeftMouseUpHandler(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            Matrix m = new Matrix();
-            m.Scale(1.1d, 1.1d);
-            ((InkCanvas)sender).Strokes.Transform(m, true);
+            // If there are no strokes stored, end here.
+            if (myCanvas.Strokes.Count == 0)
+                return;
+
+            // Get the ink line so it can be utilized.
+            Stroke curStroke = myCanvas.Strokes[0];
+
+            // Step 1 - Split the stroke into N points.
+
+            // Step 2 - Rotate so the angle between the 1st point and centroid is zero.
+
+            // Step 3 - Scale the points to fit in a boundary square.
+
+            // Step 4 - Compare each point set to several predetermined and determine the closest.
+
+            // Step 5 - Calculate a score for this predetermined and inform the user.
+
+            // Step 6 - Delete the ink line.
+            myCanvas.Strokes.Clear();
         }
     }
 }
